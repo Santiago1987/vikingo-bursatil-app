a1263c697e40bb6787bdaf6edcc91188973a6f7e
Santiago Ramos Alvarez <Santiago@Santiago-Mac.local>
2024-06-16 16:33:24 -0300

    tst

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-/*import { connectiondb } from "./mongodb";
+import { connectiondb } from "./mongodb";
 import { getLastDateRegistered } from "./controllers/especies";
 
 const connectionString = "mongodb://localhost:27017/vikingoBursatilDB";
@@ -8,36 +8,4 @@ connectiondb(connectionString);
 getLastDateRegistered("AAPL").then((res) => {
   if (!res) return;
   console.log(res.getTime());
-});*/
-
-/*import { connectiondb } from "./mongodb";
-const connectionString = "mongodb://localhost:27017/vikingoBursatilDB";
-
-connectiondb(connectionString);
-
-import { getDataFromFile } from "./utils/importCSV/getDataCSV";
-
-getDataFromFile();*/
-
-/*import { getAcciones } from "./utils/BYMAScrapper/acciones";
-getAcciones().then((res) => {
-  console.log(res);
-});*/
-import { getCedears } from "./utils/BYMAScrapper/cedears";
-import { CEDEARList } from "../Types/backend/types";
-console.log(Object.keys(CEDEARList));
-getCedears().then((res) => {
-  let cedearList = Object.keys(CEDEARList);
-
-  if (res instanceof Error) return;
-
-  for (let i = 0; i < res.length; i++) {
-    let papel = res[i];
-
-    if (cedearList.some((p) => p === papel.especie)) {
-      console.log(papel.especie);
-      if (papel.vencimiento === "24hs") {
-      }
-    }
-  }
 });
