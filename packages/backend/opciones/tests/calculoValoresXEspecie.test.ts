7a52163d08f97f6b4e66b11ed258e75515266763
Santiago-MAC <santiagora1987@gmail.com>
2025-03-03 19:39:06 -0300

    new tests

--- packages/backend/opciones/tests/calculoValoresXEspecie.test.ts
+++ packages/backend/opciones/tests/calculoValoresXEspecie.test.ts
@@ -14,93 +14,95 @@ describe("accumulateValuesBases returns an object", () => {
   });
 });
 
-describe("accumulateValuesBases returns correct values", () => {
+describe("accumulateValuesBases returns correct values fullbases", () => {
   test("accumulateValuesBases returns correct values", () => {
     let operationsList: OptionsOperations = {
       1: {
         call: [
-          { id: "1", price: 1, quantity: 1 },
-          { id: "2", price: 2, quantity: 2 },
+          { id: "1", price: 1, quantity: 5 },
+          { id: "2", price: 2, quantity: 6 },
         ],
         put: [
-          { id: "3", price: 1, quantity: 1 },
-          { id: "4", price: 2, quantity: 2 },
+          { id: "3", price: 1, quantity: 10 },
+          { id: "4", price: 2, quantity: 15 },
         ],
       },
       2: {
         call: [
-          { id: "5", price: 1, quantity: 1 },
-          { id: "6", price: 2, quantity: 2 },
+          { id: "5", price: 1, quantity: 20 },
+          { id: "6", price: 2, quantity: 10 },
         ],
         put: [
-          { id: "7", price: 1, quantity: 1 },
-          { id: "8", price: 2, quantity: 2 },
+          { id: "7", price: 1, quantity: 30 },
+          { id: "8", price: 2, quantity: 40 },
         ],
       },
     };
 
     expect(accumulateValuesBases(operationsList)).toEqual({
-      1: {
-        callPrimaTotal: 5,
-        putPrimaTotal: 5,
-        totalPrima: 10,
-        callQuantityTotal: 3,
-        putQuantityTotal: 3,
-        totalQuantity: 6,
-      },
-      2: {
-        callPrimaTotal: 5,
-        putPrimaTotal: 5,
-        totalPrima: 10,
-        callQuantityTotal: 3,
-        putQuantityTotal: 3,
-        totalQuantity: 6,
+      callPrimaTotal: 5700,
+      putPrimaTotal: 15000,
+      totalPrima: 20700,
+      basesQty: {
+        1: {
+          callQuantityTotal: 11,
+          putQuantityTotal: 25,
+        },
+        2: {
+          callQuantityTotal: 30,
+          putQuantityTotal: 70,
+        },
       },
     });
   });
 });
 
-describe("accumulateValuesBases return ", () => {
-  test("accumulateValuesBases returns correct values", () => {
+describe("accumulateValuesBases returns correct values empty bases", () => {
+  test("base 1 has no operations", () => {
     let operationsList: OptionsOperations = {
       1: {
+        call: [],
+        put: [],
+      },
+      2: {
         call: [
-          { id: "1", price: 1, quantity: 1 },
-          { id: "2", price: 2, quantity: 2 },
+          { id: "1", price: 1, quantity: 5 },
+          { id: "2", price: 2, quantity: 6 },
         ],
         put: [
-          { id: "3", price: 1, quantity: 1 },
-          { id: "4", price: 2, quantity: 2 },
+          { id: "3", price: 1, quantity: 10 },
+          { id: "4", price: 2, quantity: 15 },
         ],
       },
       3: {
         call: [
-          { id: "5", price: 1, quantity: 1 },
-          { id: "6", price: 2, quantity: 2 },
+          { id: "5", price: 1, quantity: 20 },
+          { id: "6", price: 2, quantity: 10 },
         ],
         put: [
-          { id: "7", price: 1, quantity: 1 },
-          { id: "8", price: 2, quantity: 2 },
+          { id: "7", price: 1, quantity: 30 },
+          { id: "8", price: 2, quantity: 40 },
         ],
       },
     };
 
     expect(accumulateValuesBases(operationsList)).toEqual({
-      1: {
-        callPrimaTotal: 5,
-        putPrimaTotal: 5,
-        totalPrima: 10,
-        callQuantityTotal: 3,
-        putQuantityTotal: 3,
-        totalQuantity: 6,
-      },
-      2: {
-        callPrimaTotal: 5,
-        putPrimaTotal: 5,
-        totalPrima: 10,
-        callQuantityTotal: 3,
-        putQuantityTotal: 3,
-        totalQuantity: 6,
+      callPrimaTotal: 5700,
+      putPrimaTotal: 15000,
+      totalPrima: 20700,
+      basesQty: {
+        1: {
+          callQuantityTotal: 0,
+          putQuantityTotal: 0,
+        },
+        2: {
+          callQuantityTotal: 11,
+          putQuantityTotal: 25,
+        },
+        3: {
+          callQuantityTotal: 30,
+          putQuantityTotal: 70,
+        },
       },
     });
   });
