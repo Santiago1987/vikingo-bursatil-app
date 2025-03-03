4d1cc8ed1db22d19bd765030833768bcf29d6001
Santiago-MAC <santiagora1987@gmail.com>
2025-03-03 19:05:10 -0300

    adding some test

--- packages/backend/opciones/tests/calculoValoresXEspecie.test.ts
+++ packages/backend/opciones/tests/calculoValoresXEspecie.test.ts
@@ -1,8 +1,107 @@
+import { OptionsOperations } from "../types";
 import { accumulateValuesBases } from "../utils/calculoValoresXEspecie";
-import { describe, expect, test } from "@jest/globals";
+import { beforeAll, describe, expect, test } from "@jest/globals";
 
 describe("accumulateValuesBases is a function", () => {
   test("accumulateValuesBases is a function", () => {
     expect(typeof accumulateValuesBases).toEqual("function");
+  });
+});
+
+describe("accumulateValuesBases returns an object", () => {
+  test("accumulateValuesBases returns an object", () => {
+    expect(typeof accumulateValuesBases({})).toEqual("object");
+  });
+});
+
+describe("accumulateValuesBases returns correct values", () => {
+  test("accumulateValuesBases returns correct values", () => {
+    let operationsList: OptionsOperations = {
+      1: {
+        call: [
+          { id: "1", price: 1, quantity: 1 },
+          { id: "2", price: 2, quantity: 2 },
+        ],
+        put: [
+          { id: "3", price: 1, quantity: 1 },
+          { id: "4", price: 2, quantity: 2 },
+        ],
+      },
+      2: {
+        call: [
+          { id: "5", price: 1, quantity: 1 },
+          { id: "6", price: 2, quantity: 2 },
+        ],
+        put: [
+          { id: "7", price: 1, quantity: 1 },
+          { id: "8", price: 2, quantity: 2 },
+        ],
+      },
+    };
+
+    expect(accumulateValuesBases(operationsList)).toEqual({
+      1: {
+        callPrimaTotal: 5,
+        putPrimaTotal: 5,
+        totalPrima: 10,
+        callQuantityTotal: 3,
+        putQuantityTotal: 3,
+        totalQuantity: 6,
+      },
+      2: {
+        callPrimaTotal: 5,
+        putPrimaTotal: 5,
+        totalPrima: 10,
+        callQuantityTotal: 3,
+        putQuantityTotal: 3,
+        totalQuantity: 6,
+      },
+    });
+  });
+});
+
+describe("accumulateValuesBases return ", () => {
+  test("accumulateValuesBases returns correct values", () => {
+    let operationsList: OptionsOperations = {
+      1: {
+        call: [
+          { id: "1", price: 1, quantity: 1 },
+          { id: "2", price: 2, quantity: 2 },
+        ],
+        put: [
+          { id: "3", price: 1, quantity: 1 },
+          { id: "4", price: 2, quantity: 2 },
+        ],
+      },
+      3: {
+        call: [
+          { id: "5", price: 1, quantity: 1 },
+          { id: "6", price: 2, quantity: 2 },
+        ],
+        put: [
+          { id: "7", price: 1, quantity: 1 },
+          { id: "8", price: 2, quantity: 2 },
+        ],
+      },
+    };
+
+    expect(accumulateValuesBases(operationsList)).toEqual({
+      1: {
+        callPrimaTotal: 5,
+        putPrimaTotal: 5,
+        totalPrima: 10,
+        callQuantityTotal: 3,
+        putQuantityTotal: 3,
+        totalQuantity: 6,
+      },
+      2: {
+        callPrimaTotal: 5,
+        putPrimaTotal: 5,
+        totalPrima: 10,
+        callQuantityTotal: 3,
+        putQuantityTotal: 3,
+        totalQuantity: 6,
+      },
+    });
   });
 });
