4c6cea42a475c2629863512d3983b4c00770031f
Santiago <santiagora1987@gmail.com>
2025-03-30 00:01:01 -0300

    hadding ts and jest configurations + supertest

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,15 +1,16 @@
-module.exports = {
+export default {
   testEnvironment: "node",
   transform: {
-    "^.+\\.ts$": "ts-jest",
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json", // Adjust based on your tsconfig file location
+        diagnostics: true, // Enable diagnostics for better error reporting
+      },
+    ],
   },
   transformIgnorePatterns: ["node_modules"],
   testMatch: ["**/*.test.ts"],
   extensionsToTreatAsEsm: [".ts"], // Adjust based on your file extensions
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
   preset: "ts-jest/presets/default-esm", // Adjust based on your setup
 };
