c565fd8c51e212a554f5aed8ea76111462ea6921
Santiago <santiagora1987@gmail.com>
2025-03-30 00:02:29 -0300

    index file added on especie and options folder for easy imports

---
+++ packages/backend/especies/controllers/index.ts
@@ -0,0 +1,13 @@
+import { getAllEspecies } from "./getAllEspecies";
+import { createEspecie } from "./createEspecie";
+import { getEspecie } from "./getEspecie";
+import { deleteEspecie } from "./deleteEspecie";
+import { deleteAllEspecies } from "./deleteAllEspecies";
+
+export {
+  getAllEspecies,
+  createEspecie,
+  getEspecie,
+  deleteEspecie,
+  deleteAllEspecies,
+};
---
+++ packages/backend/routes/index.ts
@@ -0,0 +1,4 @@
+import especiesRouter from "./especiesRouter";
+import optionsRouter from "./optionsRouter";
+
+export { especiesRouter, optionsRouter };
